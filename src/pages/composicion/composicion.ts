e78babc58501c1283e422ed2e41a2145cee6842c
Miguel Valero <miguel.valero@upc.edu>
2019-05-15 17:50:28 +0200

    Ejercicio 21 resuelto

--- src/pages/composicion/composicion.ts
+++ src/pages/composicion/composicion.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { HttpClient } from '@angular/common/http';
 import { SeleccionadosProvider } from '../../providers/seleccionados/seleccionados';
+import { Http, RequestOptions, Headers, Response, ResponseContentType } from '@angular/http';
 
 
 /**
@@ -21,11 +22,14 @@ export class ComposicionPage {
   idEquipo: number;
   lista: any[];
   equipo: any;
+
+  imagenLogo: string;
+
   private APIUrlBuscar = 'http://147.83.118.92:3000/api/personas?filter[where][idEquipo]=';
   private APIUrlEquipo = 'http://147.83.118.92:3000/api/equipos/';
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
-              private http: HttpClient,private proveedor : SeleccionadosProvider) {
+              private http: HttpClient, private http2: Http, private proveedor : SeleccionadosProvider) {
 
     this.idEquipo = navParams.get('id');
     console.log ('CONSTRUCTOR mostrar: ' + this.idEquipo);
@@ -51,9 +55,35 @@ export class ComposicionPage {
                             this.equipo = equipo;
                             console.log ('Ya tengo el equipo');
                             console.log (this.equipo);
+                            this.http2.get('http://localhost:3000/apI/imagenes/LogosEquipos/download/' + this.equipo.logo,
+                            { responseType: ResponseContentType.Blob } )
+                            .subscribe(response => this.CargarLogo(response));
                             this.DameAlumnos();
                           }
               );
+  }
+  CargarLogo (response: Response)
+  {
+
+      const blob = new Blob([response.blob()], { type: 'image/jpg' });
+
+      // La siguiente sentencia es la que necesitaríamos si quisieramos descargar la imagen en la carpeta de descargas
+      // saveAs(blob, this.file.name);
+
+
+      // Ahora vamos a colocar la imagen que está en blob en la etiqueta img correspondiente
+      const reader = new FileReader();
+      reader.addEventListener('load', () => {
+        // Pongo a la espera al reader de manera que en cuanto acabe coloca la URL donde toca para que se vea la imagen
+          this.imagenLogo = reader.result.toString();
+      }, false);
+
+      // Aqui es donde ordeno que se lea la imagen y se prepare la URL
+      if (blob) {
+          reader.readAsDataURL(blob);
+      }
+
+
   }
 
   Seleccionar (alumno: any) {
